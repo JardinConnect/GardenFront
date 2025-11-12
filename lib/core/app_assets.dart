d32544855b7a13728078d8a2c4a5159faec70013
Baptiste Salaud <89337396+BuSHYoff@users.noreply.github.com>
2025-11-12 20:52:50 +0100

    feat: theme data and menu sidebar (#15)
    * feat: theme data and menu sidebar
    
    * feat: garden ui rename and add basic assets
    
    * feat: menu page add asset logo
    
    * fix: https github to pass pipeline
    
    * fix: https github to pass pipeline garden ui

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String activity = 'assets/activity.svg';
+  static const String comparison = 'assets/comparison.svg';
+  static const String hexagon = 'assets/hexagon.svg';
+  static const String radio = 'assets/radio.svg';
+  static const String logo = 'assets/logo.png';
+}
--- lib/menu/view/menu_page.dart
+++ lib/menu/view/menu_page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:garden_connect/core/app_assets.dart';
 import 'package:garden_connect/dashboard/view/dashboard_page.dart';
 import 'package:garden_connect/settings/view/settings_page.dart';
 import 'package:garden_connect/menu/cubit/navigation_cubit.dart';
 import 'package:garden_connect/menu/cubit/navigation_state.dart';
+import 'package:garden_connect/auth/auth.dart';
+import 'package:garden_ui/ui/components.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
@@ -18,20 +21,55 @@ class MenuPage extends StatelessWidget {
     return BlocBuilder<NavigationCubit, NavigationState>(
       builder: (context, state) {
         return Scaffold(
-          body: _pages[state.index],
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: state.index,
-            onTap: (index) =>
-                context.read<NavigationCubit>().navigateTo(index),
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard),
-                label: 'Dashboard',
+          body: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Menu(
+                  items: [
+                    MenuItem(
+                      icon: Icons.home,
+                      label: 'Accueil',
+                      onTap:
+                          () => context.read<NavigationCubit>().navigateTo(0),
+                    ),
+                    MenuItem(
+                      icon: Icons.hexagon_outlined,
+                      label: 'Espaces',
+                      onTap: () => {},
+                    ),
+                    MenuItem(
+                      icon: Icons.sensors,
+                      label: 'Cellules',
+                      onTap: () => {},
+                    ),
+                    MenuItem(
+                      icon: Icons.thunderstorm_outlined,
+                      label: 'Alertes',
+                      onTap: () => {},
+                    ),
+                  ],
+                  bottomItems: [
+                    MenuItem(
+                      icon: Icons.settings,
+                      label: 'Paramètres',
+                      onTap:
+                          () => context.read<NavigationCubit>().navigateTo(1),
+                    ),
+                    MenuItem(
+                      icon: Icons.logout,
+                      label: 'Déconnexion',
+                      onTap:
+                          () => context.read<AuthBloc>().add(
+                            AuthLogoutRequested(),
+                          ),
+                      severity: MenuItemSeverity.danger,
+                    ),
+                  ],
+                  logo: AssetImage(AppAssets.logo),
+                ),
               ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                label: 'Settings',
-              ),
+              Expanded(flex: 8, child: _pages[state.index]),
             ],
           ),
         );
