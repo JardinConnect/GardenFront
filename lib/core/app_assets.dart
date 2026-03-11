f09db5220bb7dafb8ce2c16bb7db60d77aca851a
Mohamed Azdoud <103521660+mohamed-azd@users.noreply.github.com>
2026-03-12 00:28:53 +0100

    feat(login): add illustrations to login page (#63)

--- lib/core/app_assets.dart
+++ lib/core/app_assets.dart
@@ -4,4 +4,6 @@ class AppAssets {
   static const String hexagon = 'assets/hexagon.svg';
   static const String radio = 'assets/radio.svg';
   static const String logo = 'assets/logo.png';
+  static const String loginBackground = 'assets/login_background.svg';
+  static const String loginCharacters = 'assets/login_characters.svg';
 }
