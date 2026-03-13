3d2f52187528cc2ddbb64e51ddf79cae05c686a3
BuSHYoff <baptiste.salaud@gmail.com>
2026-03-13 12:24:28 +0100

    fix: httpClient all features

--- lib/auth/data/auth_repository.dart
+++ lib/auth/data/auth_repository.dart
@@ -1,20 +1,19 @@
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
-import 'package:http/http.dart' as http;
+import 'package:garden_connect/auth/utils/http_client.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import '../models/user.dart';
 
 class AuthRepository {
-  static const String baseUrl = 'http://127.0.0.1:8000';
+  final HttpClient _httpClient = HttpClient();
   final FlutterSecureStorage _secureStorage = const FlutterSecureStorage();
 
   Future<User?> login(String email, String password) async {
     try {
-      final response = await http.post(
-        Uri.parse('$baseUrl/login'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'email': email, 'password': password}),
+      final response = await _httpClient.post(
+        "/login",
+        body: {'email': email, 'password': password},
       );
 
       if (response.statusCode == 200) {
@@ -67,16 +66,21 @@ class AuthRepository {
       final refreshToken = await getRefreshToken();
       if (refreshToken == null) return false;
 
-      final response = await http.post(
-        Uri.parse('$baseUrl/refresh-token'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'refresh_token': refreshToken}),
+      final response = await _httpClient.post(
+        "/refresh-token",
+        body: {'refresh_token': refreshToken},
       );
 
       if (response.statusCode == 200) {
         final responseData = jsonDecode(response.body);
-        await _secureStorage.write(key: 'auth_token', value: responseData['access_token']);
-        await _secureStorage.write(key: 'refresh_token', value: responseData['refresh_token']);
+        await _secureStorage.write(
+          key: 'auth_token',
+          value: responseData['access_token'],
+        );
+        await _secureStorage.write(
+          key: 'refresh_token',
+          value: responseData['refresh_token'],
+        );
         return true;
       }
       return false;
