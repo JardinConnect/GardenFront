a83df522924727ff48633854a642a53021599c84
BuSHYoff <baptiste.salaud@gmail.com>
2026-03-13 16:10:50 +0100

    fix: flutter build web

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/widgets.dart';
 import 'package:garden_connect/app.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  GoogleFonts.config.allowRuntimeFetching = false;
   await initializeDateFormatting('fr_FR', null);
   runApp(const App());
 }
