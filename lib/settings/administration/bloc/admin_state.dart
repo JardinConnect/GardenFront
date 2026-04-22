8c578bf000a3dad26b627fe199c7e23beeda5b89
Mohamed Azdoud <103521660+mohamed-azd@users.noreply.github.com>
2026-04-22 19:58:47 +0200

    feat(Admin): connect system metrics to the backend (#113)

---
+++ lib/settings/administration/bloc/admin_state.dart
@@ -0,0 +1,27 @@
+part of 'admin_bloc.dart';
+
+@immutable
+sealed class AdminState {
+
+  const AdminState();
+}
+
+final class AdminInitial extends AdminState {
+  const AdminInitial() : super();
+}
+
+final class AdminError extends AdminState {
+  final String message;
+
+  const AdminError({required this.message}) : super();
+}
+
+final class AdminShimmer extends AdminState {
+  const AdminShimmer() : super();
+}
+
+final class AdminLoaded extends AdminState {
+  final SystemMetrics systemMetrics;
+
+  const AdminLoaded({required this.systemMetrics});
+}
--- lib/settings/administration/pages/administration_page.dart
+++ lib/settings/administration/pages/administration_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:garden_connect/auth/utils/auth_extension.dart';
 import 'package:garden_connect/common/widgets/page_header.dart';
+import 'package:garden_connect/common/widgets/page_shimmers.dart';
+import 'package:garden_connect/settings/administration/bloc/admin_bloc.dart';
 import 'package:garden_connect/settings/administration/widgets/admin_danger_zone_widget.dart';
 import 'package:garden_connect/settings/administration/widgets/cgu_widget.dart';
 import 'package:garden_connect/settings/administration/widgets/raspberry_infos_widget.dart';
@@ -19,44 +22,56 @@ class AdministrationPage extends StatelessWidget {
       return const Text("Utilisateur non connecté");
     }
 
-    return Padding(
-      padding: EdgeInsets.all(GardenSpace.paddingMd),
-      child: SingleChildScrollView(
-        child: Column(
-          spacing: GardenSpace.gapMd,
-          children: [
-            PageHeader(title: "Bonjour ${user.firstName}"),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: GardenSpace.gapMd,
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: Column(
-                    spacing: GardenSpace.gapSm,
+    return BlocBuilder<AdminBloc, AdminState>(
+      builder: (context, state) {
+        if (state is AdminInitial || state is AdminShimmer) {
+          return const CellsPageShimmer();
+        } else if (state is AdminError) {
+          return Center(child: Text('Erreur: ${state.message}'));
+        } else if (state is AdminLoaded) {
+          return Padding(
+            padding: EdgeInsets.all(GardenSpace.paddingMd),
+            child: SingleChildScrollView(
+              child: Column(
+                spacing: GardenSpace.gapMd,
+                children: [
+                  PageHeader(title: "Bonjour ${user.firstName}"),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: GardenSpace.gapMd,
                     children: [
-                      SystemInfosWidget(),
-                      SavesWidget(),
-                      AdminDangerZoneWidget()
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          spacing: GardenSpace.gapSm,
+                          children: [
+                            SystemInfosWidget(),
+                            SavesWidget(),
+                            AdminDangerZoneWidget(),
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Column(
+                          spacing: GardenSpace.gapSm,
+                          children: [
+                            RaspberryInfosWidget(systemMetrics: state.systemMetrics),
+                            SystemToolsWidget(),
+                            CGUWidget(),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Column(
-                    spacing: GardenSpace.gapSm,
-                    children: [
-                      RaspberryInfosWidget(),
-                      SystemToolsWidget(),
-                      CGUWidget(),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return const Center(child: Text('Erreur'));
+        }
+      },
     );
   }
 }
